395a68aafb27c9674c380236a89e22f125e1420a
malpartida flores bryan Anthony <bmalpartida@ws22.labo2>
2019-04-09 16:08:37 -0300

    Cargo unos arreglos al codigo del teorema C

--- Calculos Numéricos/Codigos/Entangelment-Cth.py
+++ Calculos Numéricos/Codigos/Entangelment-Cth.py
@@ -5,13 +5,13 @@ from numpy import linalg as LA
 def Msqrt(M): ### Método de Newton-Raphson de la raiz cuadrada.
     x = M;
     e = 1;
-    while(e > 10E-7):
+    while(e > 10E-8):
         x = 0.5*x+0.5*np.dot(M,LA.inv(x));
         e = LA.norm(np.dot(x,x)-M);
     return x   
 
-n = 10000;
 #m = 0.1;
+n = 10000;
 x = np.zeros(n);
 for i in range(n):
   x[n-i-1]=np.pi*np.cos((2*i+1)*np.pi/(2*n));
@@ -34,82 +34,94 @@ def Int(k):
         J = J + ((b-a)/6)*(g(a,k)+4*g((a+b)/2,k)+g(b,k));
     return I,J
 
-res1 = [];
-res2 = [];
-for k in range(50):
-    aux1,aux2 = Int(k);
-    res1.append(aux1);
-    res2.append(aux2);
+# Rmax = 50;
+# res1 = [];
+# res2 = [];
+# for k in range(Rmax):
+#     aux1,aux2 = Int(k);
+#     res1.append(aux1);
+#     res2.append(aux2);
 
-R = 50;
-S = [];
-for r in range(1,R):
-    X = np.zeros((r,r));
-    P = np.zeros((r,r));
-    for i in range(r):
-        for j in range(i,r):
-            X[i,j] = res1[j-i];
-            X[j,i] = X[i,j];
-            P[i,j] = res2[j-i];
-            P[j,i] = P[i,j];
-    Cv = Msqrt(np.dot(X,P));
-    d1 = LA.eigvals(Cv+0.5*np.eye(len(Cv)));
-    d2 = LA.eigvals(Cv-0.499999*np.eye(len(Cv)));
-    S.append(np.dot(d1,np.log(d1))-np.dot(d2,np.log(d2)));
+# R = 50;
+# S = [];
+# for r in range(1,R):
+#     X = np.zeros((r,r));
+#     P = np.zeros((r,r));
+#     for i in range(r):
+#         for j in range(i,r):
+#             X[i,j] = res1[j-i];
+#             X[j,i] = X[i,j];
+#             P[i,j] = res2[j-i];
+#             P[j,i] = P[i,j];
+#     Cv = Msqrt(np.dot(X,P));
+#     d1 = LA.eigvals(Cv+0.5*np.eye(len(Cv)));
+#     d2 = LA.eigvals(Cv-0.499999*np.eye(len(Cv)));
+#     S.append(np.dot(d1,np.log(d1))-np.dot(d2,np.log(d2)));
     
-#p,o = np.polyfit(range(2,R),S,1);
-    
-plt.plot(range(1,len(S)+1),S,'o-')
-plt.show()
+#p,o = np.polyfit(range(2,R),S,1);   
+# plt.plot(range(1,len(S)+1),S,'o-')
+# plt.show()
 
 #np.savez('EE-cth-0.1',S,R,m,x)
-Masas = np.linspace(0.01,1,10);
-Sm=[];
-for i in range(len(Masas)):
-  m = Masas[i];
-  R = int(10/Masas[i]);
-  res1 = [];
-  res2 = [];
-  for k in range(R):
+# Masas = np.linspace(0.01,1,10);
+# Sm=[];
+# for i in range(len(Masas)):
+#   m = Masas[i];
+#   R = int(10/Masas[i]);
+#   res1 = [];
+#   res2 = [];
+#   for k in range(R):
+#     aux1,aux2 = Int(k);
+#     res1.append(aux1);
+#     res2.append(aux2);
+#   X = np.zeros((R,R));
+#   P = np.zeros((R,R));
+#   for k in range(R):
+#     for j in range(k,R):
+#       X[k,j] = res1[j-k];
+#       X[j,k] = X[k,j];
+#       P[k,j] = res2[j-k];
+#       P[j,k] = P[k,j];
+#   Cv = Msqrt(np.dot(X,P));
+#   d1 = LA.eigvals(Cv+0.5*np.eye(len(Cv)));
+#   d2 = LA.eigvals(Cv-0.499999*np.eye(len(Cv)));
+#   Sm.append(np.dot(d1,np.log(d1))-np.dot(d2,np.log(d2)));
+  
+# plt.plot(np.log(Masas),Sm,'o-')
+# plt.show()
+# p,o = np.polyfit(np.log(Masas),Sm,1)
+#np.savez('EE-cth-masas',Sm,Masas,x)
+
+m = 10E-7;
+Rmax = 100;
+res1 = [];
+res2 = [];
+for k in range(Rmax):
     aux1,aux2 = Int(k);
     res1.append(aux1);
     res2.append(aux2);
-  X = np.zeros((R,R));
-  P = np.zeros((R,R));
-  for k in range(R):
-    for j in range(k,R):
-      X[k,j] = res1[j-k];
-      X[j,k] = X[k,j];
-      P[k,j] = res2[j-k];
-      P[j,k] = P[k,j];
-  Cv = Msqrt(np.dot(X,P));
-  d1 = LA.eigvals(Cv+0.5*np.eye(len(Cv)));
-  d2 = LA.eigvals(Cv-0.499999*np.eye(len(Cv)));
-  Sm.append(np.dot(d1,np.log(d1))-np.dot(d2,np.log(d2)));
-  
-plt.plot(np.log(Masas),Sm,'o-')
-plt.show()
 
-p,o = np.polyfit(np.log(Masas),Sm,1)
+# X = np.zeros((Rmax,Rmax));
+# P = np.zeros((Rmax,Rmax));
 
-#np.savez('EE-cth-masas',Sm,Masas,x)
+# for i in range(Rmax):
+# 	for j in range(i,Rmax):
+# 		X[i,j] = res1[j-i];
+# 		X[j,i] = X[i,j];
+# 		P[i,j] = res2[j-i];
+# 		P[j,i] = P[i,j];
 
-m = 10E-10;
-R = 50;
-S = [];
-for r in range(10,R):
-    X = np.zeros((r,r));
-    P = np.zeros((r,r));
-    for i in range(r):
-      for j in range(i,r):
-        X[i,j] = res1[j-i];
-        X[j,i] = X[i,j];
-        P[i,j] = res2[j-i];
-        P[j,i] = P[i,j];
-    Cv = Msqrt(np.dot(X,P));
-    d1 = LA.eigvals(Cv+0.5*np.eye(len(Cv)));
-    d2 = LA.eigvals(Cv-0.499999*np.eye(len(Cv)));
-    S.append(np.dot(d1,np.log(d1))-np.dot(d2,np.log(d2)));
-    
+plt.plot(res1,'o-')
 
-    
+# S = [];
+
+# for r in range(10,Rmax):
+# 	Xv = X[0:r,0:r];
+# 	Pv = P[0:r,0:r];
+# 	Cv = Msqrt(np.dot(Xv,Pv));
+# 	d1 = LA.eigvals(Cv+0.5*np.eye(len(Cv)));
+# 	d2 = LA.eigvals(Cv-0.499999*np.eye(len(Cv)));
+# 	S.append(np.dot(d1,np.log(d1))-np.dot(d2,np.log(d2)));
+
+# plt.plot(range(10,Rmax),S,'o-')
+# plt.show()
---
+++ Calculos Numéricos/Codigos/x.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+x = [];
+for i in range(10):
+	x.append(i);
+
+plt.plot(x,'o-')
+plt.show()
